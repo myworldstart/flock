daecac684e6127be7451e10ec84821bb1bfbfec4
xjb <2508611101@qq.com>
2024-02-01 22:14:47 +0800

    完成了normalize staing

--- nuclei_seg/staining_normalization/normalizeStaining.py
+++ nuclei_seg/staining_normalization/normalizeStaining.py
@@ -1,7 +1,8 @@
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-def normalizeStaining(I, Io, beta, alpha, HERef, maxCRef):
+def normalizeStaining(I, Io=None, beta=None, alpha=None, HERef=None, maxCRef=None):
 
     if 'Io' not in locals() or not Io:
         Io = 240
@@ -20,13 +21,12 @@ def normalizeStaining(I, Io, beta, alpha, HERef, maxCRef):
         ])
 
     if 'maxCRef' not in locals() or maxCRef is None:
-        maxCRef = np.array([1.9705, 1.0308])
+        maxCRef = np.array([[1.9705], [1.0308]])
 
-    h = I.size(0)
-    w = I.size(1)
+    h,w,_ = I.shape
 
     I = I.astype(np.float64)
-    I = np.reshape(I, (-1, 3))
+    I = np.reshape(I, (-1, 3), order="F")
 
     OD = -1*np.log((I + 1) / Io)
 
@@ -38,14 +38,14 @@ def normalizeStaining(I, Io, beta, alpha, HERef, maxCRef):
     D = D[ind]
     V = V[:,ind]
 
-    That = ODhat*V[:, 1:3]
+    That = np.dot(ODhat, V[:, 1:3])
     phi = np.arctan2(That[:,1], That[:,0])
 
     minPhi = np.percentile(phi, alpha, interpolation='lower')
     maxPhi = np.percentile(phi, 100 - alpha, interpolation='lower')
 
-    vMin = np.dot(V[:, 1:3], np.array([np.cos(minPhi), np.sin(minPhi)]))
-    vMax = np.dot(V[:, 1:3], np.array([np.cos(maxPhi), np.sin(maxPhi)]))
+    vMin = np.dot(V[:, 1:3], np.array([[np.cos(minPhi)], [np.sin(minPhi)]]))
+    vMax = np.dot(V[:, 1:3], np.array([[np.cos(maxPhi)], [np.sin(maxPhi)]]))
 
     if(vMin[0] > vMax[0]):
         HE = np.hstack((vMin, vMax))
@@ -54,12 +54,44 @@ def normalizeStaining(I, Io, beta, alpha, HERef, maxCRef):
 
     Y = np.reshape(OD, (-1, 3)).T
 
-    C = np.dot(np.linalg.inv(HE), Y)
-    maxC = np.percentile(C, 99, 2, interpolation='lower')
+    C = np.linalg.lstsq(HE, Y, rcond=None)[0]
+    maxC = np.percentile(C, 99, 1, interpolation='lower')
+    maxC = np.reshape(maxC, (-1, 1))
 
-    np.divide(C, maxC, C)
+    C = np.divide(C, maxC)
     C = C * maxCRef
+
     Inorm = Io * np.exp(-np.dot(HERef, C))
+    Inorm = np.reshape(Inorm.T, (h, w, 3))
+    Inorm = Inorm.astype(int)
+
+    HERef1 = -HERef[:, 1]
+    HERef1 = np.reshape(HERef1, (-1, 1))
+    C1 = C[1, :]
+    C1 = np.reshape(C1, (1, -1))
+
+    H = Io * np.log(np.dot(HERef1, C1))
+    H = np.reshape(H.T, (h, w, 3))
+    H = H.astype(int)
+
+    E = Io * np.log(np.dot(HERef1, C1))
+    E = np.reshape(E.T, (h, w, 3))
+    E = E.astype(int)
+
+    return Inorm, H, E
+
+
+# if __name__ == '__main__':
+#     img = cv2.imread('/home/xjb/Code1/matlab/flock/img/TMA 002-G6.png')
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     crop_img = img[1132:1132+415, 623:623+511, :]
+#     print(crop_img[0, 0, :])
+#     plt.figure()
+#     plt.imshow(crop_img)
+#     plt.axis('off')
+#     plt.show()
+#     Inorm, _, _ = normalizeStaining(crop_img)
+
 
 
 
